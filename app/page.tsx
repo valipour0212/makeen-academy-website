9b9905e5fe62dbdf90ab90582a00fa28cc8cfdac
Mohammad Valipour <valipour2001@gmail.com>
2025-07-02 16:32:43 +0330

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HomePage from "@/components/pages/Home/HomePage";
 function Home() {
     return (
         <main className="w-full h-screen bg-[#F8F8F8] py-[74px] flex flex-col items-center">
-            <HomePage/>
+            <HomePage />
         </main>
     );
 }
